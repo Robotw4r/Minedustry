ae17cba02cd3a3bb5a6b8ed7800f797bd336fc32
Swiiz <Megaport@Swiiz>
2020-05-03 21:21:35 +0200

    blocks

--- src/main/java/org/minedustry/blocks/BlockBase.java
+++ src/main/java/org/minedustry/blocks/BlockBase.java
@@ -1,4 +1,13 @@
 package org.minedustry.blocks;
 
-public class BlockBase {
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class BlockBase extends Block {
+	
+	public BlockBase() {
+		super(Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(2.0f));
+	}
+	
 }
--- src/main/java/org/minedustry/utilities/RegistryHandler.java
+++ src/main/java/org/minedustry/utilities/RegistryHandler.java
@@ -1,9 +1,12 @@
 package org.minedustry.utilities;
 
+import org.minedustry.MinedustryMain;
 import org.minedustry.References;
+import org.minedustry.blocks.BlockBase;
 import org.minedustry.items.ItemBase;
 import net.minecraft.block.Block;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -27,4 +30,10 @@ public class RegistryHandler {
     public static final RegistryObject<Item> THORIUM_INGOT = ITEMS.register("thorium_ingot", ItemBase::new);
 
     //List of mod blocks
+    public static final RegistryObject<Block> PLATINUM_BLOCK = BLOCKS.register("platinum_block", BlockBase::new);
+    public static final RegistryObject<Item> PLATINUM_BLOCK_ITEM = ITEMS.register("platinum_block", () -> new BlockItem(PLATINUM_BLOCK.get(), new Item.Properties().group(MinedustryMain.TAB)));
+    public static final RegistryObject<Block> COBALT_BLOCK = BLOCKS.register("cobalt_block", BlockBase::new);
+    public static final RegistryObject<Item> COBALT_BLOCK_ITEM = ITEMS.register("cobalt_block", () -> new BlockItem(COBALT_BLOCK.get(), new Item.Properties().group(MinedustryMain.TAB)));
+    public static final RegistryObject<Block> THORIUM_BLOCK = BLOCKS.register("thorium_block", BlockBase::new);
+    public static final RegistryObject<Item> THORIUM_BLOCK_ITEM = ITEMS.register("thorium_block", () -> new BlockItem(THORIUM_BLOCK.get(), new Item.Properties().group(MinedustryMain.TAB)));
 }
