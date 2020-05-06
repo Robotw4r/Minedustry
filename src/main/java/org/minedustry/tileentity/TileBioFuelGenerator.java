9c2238a9dab52d5564d3540922fcb4ba537de32f
Swiiz <Megaport@Swiiz>
2020-05-06 13:18:05 +0200

    useless class removed

--- src/main/java/org/minedustry/tileentity/TileBioFuelGenerator.java
+++ src/main/java/org/minedustry/tileentity/TileBioFuelGenerator.java
@@ -6,93 +6,79 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 
-public class TileBioFuelGenerator extends TileEntityStorage
-{	
-	public TileBioFuelGenerator()
-	{
+public class TileBioFuelGenerator extends TileEntityStorage {	
+	public TileBioFuelGenerator() {
 		super(TileEntityRegistry.BIO_FUEL_GENERATOR);
 	}
 	
 	@Override
-	public int[] getSlotsForFace(Direction side)
-	{
+	public int[] getSlotsForFace(Direction side) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean canInsertItem(int index, ItemStack itemStackIn, Direction direction)
-	{
+	public boolean canInsertItem(int index, ItemStack itemStackIn, Direction direction) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean canExtractItem(int index, ItemStack stack, Direction direction)
-	{
+	public boolean canExtractItem(int index, ItemStack stack, Direction direction) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public int getSizeInventory()
-	{
+	public int getSizeInventory() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public boolean isEmpty()
-	{
+	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public ItemStack decrStackSize(int index, int count)
-	{
+	public ItemStack decrStackSize(int index, int count) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ItemStack removeStackFromSlot(int index)
-	{
+	public ItemStack removeStackFromSlot(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setInventorySlotContents(int index, ItemStack stack)
-	{
+	public void setInventorySlotContents(int index, ItemStack stack) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public boolean isUsableByPlayer(PlayerEntity player)
-	{
+	public boolean isUsableByPlayer(PlayerEntity player) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void clear()
-	{
+	public void clear() {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public int getSlotLimit(int slot)
-	{
+	public int getSlotLimit(int slot) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int getSlots()
-	{
+	public int getSlots() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
