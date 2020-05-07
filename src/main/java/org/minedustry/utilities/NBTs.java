c3c46a95c8ef1f04ced69468e0ac3085024b7645
Eno_gamer10 <enor.vrai@gmail.com>
2020-05-07 12:14:18 +0200

    Added NBTs for tile entities

---
+++ src/main/java/org/minedustry/utilities/NBTs.java
@@ -0,0 +1,9 @@
+package org.minedustry.utilities;
+
+public class NBTs
+{
+	public static final String ENERGY = "Energy";
+	public static final String MAX_ENERGY = "Capacity";
+	public static final String MAX_INSERT = "InsertCapacity";
+	public static final String MAX_EXTRACT = "ExtractCapacity";
+}
