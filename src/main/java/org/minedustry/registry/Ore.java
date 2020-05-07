c83cf248a1154dd2cf84f41352abeb2a8e8459c3
Swiiz <Megaport@Swiiz>
2020-05-07 11:48:21 +0200

    ore registery

---
+++ src/main/java/org/minedustry/registry/Ore.java
@@ -0,0 +1,49 @@
+package org.minedustry.registry;
+
+import org.minedustry.blocks.BlockBase;
+import org.minedustry.items.ItemBase;
+import org.minedustry.items.ItemBlock;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+public class Ore {
+	
+	private final String name;
+	private final Item item;
+	private final Block block;
+	private final Item itemBlock;
+
+	public Ore(String name) {
+		this.name = name;
+		
+		item = new ItemBase(name + "_ingot");
+		block = new BlockBase(name + "_block");
+		itemBlock = new ItemBlock(block);
+	}
+	
+	public Ore(String name, String itemSuffix) {
+		this.name = name;
+		
+		item = new ItemBase(name + "_" + itemSuffix);
+		block = new BlockBase(name + "_block");
+		itemBlock = new ItemBlock(block);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	
+	public Block getBlock() {
+		return block;
+	}
+	
+	public Item getItemBlock() {
+		return itemBlock;
+	}
+
+}
