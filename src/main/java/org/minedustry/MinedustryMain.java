b2a97eda7ba953a5f1134ce83f52a577a33ce78b
Eno_gamer10 <enor.vrai@gmail.com>
2020-05-07 12:02:40 +0200

    few changes

--- src/main/java/org/minedustry/MinedustryMain.java
+++ src/main/java/org/minedustry/MinedustryMain.java
@@ -22,7 +22,7 @@ public class MinedustryMain {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) { }
+    private void setup(final FMLCommonSetupEvent event) {}
 
     private void doClientStuff(final FMLClientSetupEvent event) { new ClientRegistry(); }
 }
